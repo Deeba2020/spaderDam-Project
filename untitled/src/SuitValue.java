a823f563d089f1e4d342ad4a596c63aac53b2f89
samra-iths <samra.assamnew.andargie@iths.se>
2023-11-14 11:32:28 +0100

    Added suitvalue charater and values for the each suitvalue. now we can compare their values.

--- untitled/src/SuitValue.java
+++ untitled/src/SuitValue.java
@@ -1,17 +1,34 @@
 
 //make this one unicode
 public enum SuitValue {
-    TWO ,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING,
-    ACES;
+    TWO("2", 2) ,
+    THREE("3",3),
+    FOUR("4", 4),
+    FIVE ("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 11),
+    QUEEN("Q", 12),
+    KING("K", 13),
+    ACES("A", 14);
+    
+
+private  String rank;
+private int rankValue;
+
+private SuitValue(String rank, int rankValue) {
+        this.rank = rank;
+        this.rankValue= rankValue;
+    }
+
+public String getSuitChar() {
+            return rank;
+          }
+public int getRankValue(){
+    return rankValue;
 }
+        }
+    
