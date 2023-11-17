02289846553fd2c1bf13d31b75584d0e6e7866a8
Deeba2020 <71167891+Deeba2020@users.noreply.github.com>
2023-11-17 02:32:41 +0100

    modified AI classes and main

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -10,32 +10,8 @@ public class Main {
 
 
         System.out.println("name of the active player is "+ userName);
-//
-//        System.out.println("round one");
 
-//        Player p1 =new Player("a");
-//        Player p2 = new Player("b");
-//        Player p3 = new Player("C");
-//        Player p4 = new Player("d");
-//
-//        Dealer dealer = new Dealer();
-//
-//        dealer.addPlayer(p1);
-//        dealer.addPlayer(p2);
-//        dealer.addPlayer(p3);
-//        dealer.addPlayer(p4);
-//
-//        dealer.dealCards(p1,p2,p3,p4);
-//
-//        p1.displayHand();
-//        p2.displayHand();
-//        p3.displayHand();
-//        p4.displayHand();
-//
-//        p1.startPlay(p1);
-//        p2.startPlay(p2);
-//        p3.startPlay(p3);
-//        p4.startPlay(p4);
+//        System.out.println("round one");
 
 
         ActivePlayer a = new ActivePlayer(userName);
